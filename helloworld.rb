017e2e7a92be322984714f7b10ffe87d84220a68
uma-git413 <kazu.umauri@gmail.com>
2019-12-15 11:01:25 +0900

    hello world

---
+++ helloworld.rb
@@ -0,0 +1,27 @@
+puts "Hello World  ccc"
+
+age = 18
+if age < 20 
+	puts "あなたは未成年です。"
+end
+
+age = 18
+puts "あなたは未成年です" if age < 20 
+
+
+for num in [1,2,3,4] do
+  p num
+end
+
+array = ["Ruby", "Rails", "Swift", "Java", "PHP"]
+p array[1+2]
+
+
+num = 0
+sum = 1
+while num < 10	
+	num += 1
+	sum += sum
+end
+	p sum
+
